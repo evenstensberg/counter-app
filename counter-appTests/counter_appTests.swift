b5df8032302743cde380f5fb21982d18b786ea95
ev1stensberg <evenstensberg@gmail.com>
2019-06-11 18:08:25 +0200

    chore: init

---
+++ counter-appTests/counter_appTests.swift
@@ -0,0 +1,34 @@
+//
+//  counter_appTests.swift
+//  counter-appTests
+//
+//  Created by Even Stensberg on 11/06/2019.
+//  Copyright © 2019 Even Stensberg. All rights reserved.
+//
+
+import XCTest
+@testable import counter_app
+
+class counter_appTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
